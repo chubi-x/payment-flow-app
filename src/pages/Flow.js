151bf18c7179f6124dccecd797791b90f31771e3
chubi-x <chubixax@protonmail.com>
2022-08-20 12:30:26 +0100

    feat: import checkout steps header to flow page

--- src/pages/Flow.js
+++ src/pages/Flow.js
@@ -1,6 +1,12 @@
 import React from "react";
-import FlowHeader from "../Components/FlowHeader";
-
+import CheckoutSteps from "../utils/CheckoutSteps";
 export default function Flow() {
-  return <FlowHeader />;
+  return (
+    <div className="flow-container container flex flex-col justify-center mx-auto w-2/3 pt-20 md:w-1/2">
+      <h1 className="text-2xl text-purple font-bold mb-4">
+        Complete your Purchase
+      </h1>
+      <CheckoutSteps />
+    </div>
+  );
 }
