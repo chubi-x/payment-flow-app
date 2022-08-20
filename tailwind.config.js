dbdc03fa1f9fddd6a7318642c6c777de9109fd59
chubi-x <chubixax@protonmail.com>
2022-08-20 20:29:03 +0100

    chore: add color to tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
       colors: {
         purple: "#4E598C",
         blueMain: "#2F80ED",
+        lightBlue: "#817E9E",
         yellow1: "#F2C94C",
         yellow2: "#F2994A",
         red: "#EB5757",
