7d0d1e9c1eac24d13d445d7fd5e01da34e13ee8c
chubi-x <chubixax@protonmail.com>
2022-08-20 17:57:45 +0100

    feat: mobile styling

--- src/utils/FlowButtons.js
+++ src/utils/FlowButtons.js
@@ -1,7 +1,7 @@
 import React from "react";
 export default function FlowButtons() {
   return (
-    <div className="flow-buttons flex space-x-20">
+    <div className="flow-buttons flex flex-col md:flex-row space-x-20">
       <button className="next-button bg-gradient-to-r from-yellow1 to-yellow2">
         Next
       </button>
