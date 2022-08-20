6889ca6065e330538b1291f510a30889086e6378
chubi-x <chubixax@protonmail.com>
2022-08-20 18:46:39 +0100

    chore: tailwindcss color config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx}"],
   theme: {
-    colors: {
-      purple: "#4E598C",
-      blueMain: "#2F80ED",
-      yellow1: "#F2C94C",
-      yellow2: "#F2994A",
-      red: "#EB5757",
+    extend: {
+      colors: {
+        purple: "#4E598C",
+        blueMain: "#2F80ED",
+        yellow1: "#F2C94C",
+        yellow2: "#F2994A",
+        red: "#EB5757",
+      },
     },
-    extend: {},
   },
   plugins: [],
 };
