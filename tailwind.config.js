0d31a451918b6e3ee8eaa6f486c5ad66972b2e4d
chubi-x <chubixax@protonmail.com>
2022-08-20 10:11:51 +0100

    chore: update theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx}"],
   theme: {
+    colors: {
+      purple: "#4E598C",
+      blueMain: "#2F80ED",
+      yellow1: "#F2C94C",
+      yellow2: "#F2994A",
+      red: "#EB5757",
+    },
     extend: {},
   },
   plugins: [],
