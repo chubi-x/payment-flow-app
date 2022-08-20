780a4c7d4a4d5c26624e05dbedc096bec74a4b91
chubi-x <chubixax@protonmail.com>
2022-08-20 15:52:52 +0100

    feat: complete flow buttons styling

---
+++ src/utils/FlowButtons.js
@@ -0,0 +1,11 @@
+import React from "react";
+export default function FlowButtons() {
+  return (
+    <div className="flow-buttons flex space-x-20">
+      <button className="next-button bg-gradient-to-r from-yellow1 to-yellow2">
+        Next
+      </button>
+      <button className="text-purple font-bold">Cancel Payment</button>
+    </div>
+  );
+}
